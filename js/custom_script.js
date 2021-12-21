0e41b948d1b50aab8b148f0be546bf69bd47c75d
Karin Ljunggren <kaljunggren@gmail.com>
2021-12-21 20:04:32 +0100

    nav to sass

--- js/custom_script.js
+++ js/custom_script.js
@@ -1,56 +1,53 @@
 //hamburger script
 
+jQuery(document).ready(function ($) {
+  /*when click on veggoburger...add or remove class */
 
- jQuery(document).ready(function($) {
-     
-    /*when click on veggoburger...add or remove class */
+  //   $(".toggle").click(function () {
+  //     $(".nav_mobile").toggleClass("nav_mobile_open");
+  //   });
 
-    $('.toggle').click(function () {
-        $('.nav-mobile').toggleClass('nav-mobile-open');
-    });
-    
-     /*when click on menu-item with sub-menu...add or remove class*/
+  /*Desktop*/
+  $(".nav_desktop ul li.menu-item-has-children").click(function (event) {
+    event.stopPropagation(); /* to stop the 'document handler' from activating at the same time as the click event on class */
+    $(".nav_desktop ul li.menu-item-has-children ul").toggleClass(
+      "sub_menu_open"
+    );
+  });
+  /*remove class clicking anywhere on page*/
+  $(document).click(function (event) {
+    if (
+      !$(event.target).closest(".nav_desktop ul li.menu-item-has-children ul")
+        .length
+    ) {
+      $(".nav_desktop ul li.menu-item-has-children ul").removeClass(
+        "sub_menu_open"
+      );
+    }
+  });
 
-    /*Desktop*/
-    $('.nav-desktop ul li.menu-item-has-children').click(function (event) {
-        event.stopPropagation(); /* to stop the 'document handler' from activating at the same time as the click event on class */
-        $('.nav-desktop ul li.menu-item-has-children ul').toggleClass('sub-menu-open');
-    });
+  /*when click on veggoburger, or..add or remove class */
+  $(".toggle_btn, .nav-mobile .button_link").click(function () {
+    $(".nav_mobile").toggleClass("open");
+    $(".toggle_btn").toggleClass("clicked");
+    $("body").toggleClass("no_scroll");
+  });
 
-    /*remove class clicking anywhere on page*/
-    $(document).click(function (event) {
-        if (!$(event.target).closest('.nav-desktop ul li.menu-item-has-children ul').length) {
-            $(".nav-desktop ul li.menu-item-has-children ul").removeClass("sub-menu-open");
-        }
-    });
-
-
-    /*Mobile*/
-
-    $('.nav-mobile ul li.menu-item-has-children').click(function (event) {
-        event.stopPropagation(); /* to stop the 'document handler' from activating at the same time as the click event on class */
-        $('.nav-mobile ul li.menu-item-has-children ul').toggleClass('sub-menu-open');
-    });
-
-
-    /*remove class clicking anywhere on page*/
-    $(document).click(function (event) {
-        if (!$(event.target).closest('.nav-mobile ul li.menu-item-has-children ul').length) {
-            $(".nav-mobile ul li.menu-item-has-children ul").removeClass("sub-menu-open");
-        }
-    });
-     
-     
-    
-    //ger textwidgeten class="text-part"
-   // $('.card-container .image-box:first').addClass('image-part'); 
-     
-   
-     
-});//document.ready
-
-
-
-
-
-
+  /*Mobile*/
+  $(".nav_mobile ul li.menu-item-has-children").click(function (event) {
+    event.stopPropagation(); /* to stop the 'document handler' from activating at the same time as the click event on class */
+    $(".nav_mobile ul li.menu-item-has-children ul").toggleClass(
+      "sub_menu_open"
+    );
+  });
+  $(document).click(function (event) {
+    if (
+      !$(event.target).closest(".nav_mobile ul li.menu-item-has-children ul")
+        .length
+    ) {
+      $(".nav_mobile ul li.menu-item-has-children ul").removeClass(
+        "sub_menu_open"
+      );
+    }
+  });
+}); //document.ready
--- parts/main-menu.php
+++ parts/main-menu.php
@@ -1,50 +1,45 @@
 <!--Menu-3 rom Boozang. Desktop menu with logo and Sign up button. Mobile menu sliding in from right - covering whole screen. Sub menus. Made with Flexbox -->
 
-<nav class="nav-desktop">
-        <h2 class="site-title">
-                <a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>">
-                    ShiatsuRummet
-                </a> 
-            </h2>
-        
-        <div class="nav-links">
-            
-          <?php wp_nav_menu(); ?>
-            
-            <div class="booking-btn">
-                <ul>
-                    <li>
-                        <a href="<?php echo get_theme_mod('custom-link-booking-url'); ?>" title="bokadirekt" target="_blank">
+<nav class="nav_desktop">
+    <h2 class="site_title">
+        <a class="logo" href="<?php echo esc_url(home_url('/')); ?>">
+            ShiatsuRummet
+        </a>
+    </h2>
 
-                            <img class="bokadirekt" src="https://foretag.bokadirekt.se/bokatid/BokaTid_Gron_MorkBakgrund_120px.png" alt="Boka tid" border="0" />
-                        </a> 
-                    </li>
-                    
-                </ul>
-            </div>
+    <div class="nav_links">
+        <?php wp_nav_menu(); ?>
+
+        <div class="booking_btn">
+            <a href="<?php echo get_theme_mod('custom-link-booking-url'); ?>" title="bokadirekt" target="_blank">
+
+                <img class="bokadirekt" src="https://foretag.bokadirekt.se/bokatid/BokaTid_Gron_MorkBakgrund_120px.png"
+                    alt="Boka tid" border="0" />
+            </a>
         </div>
-
-            <span class="toggle">&#9776;</span> <!--hamburger-->
-    </nav>
-    
-    <div class="nav-mobile">
-          <span class="toggle">&times;</span>  <!--close-->
-            
-          <div class="nav-links">
-            
-          <?php wp_nav_menu(); ?>
-            
-            <div class="signup">
-                <ul>
-                    <li>
-                        <a href="<?php echo get_theme_mod('custom-link-booking-url'); ?>" title="bokadirekt" target="_blank">
-
-                             <img class="bokadirekt" src="https://foretag.bokadirekt.se/bokatid/BokaTid_Vit_MorkBakgrund_120px.png" alt="Boka tid" border="0" />
-                        </a> 
-                    </li>
-                    
-                </ul>
-            </div>
-        </div>
-       
     </div>
+
+    <!-- <span class="toggle">&#9776;</span> -->
+    <!--hamburger-->
+    <div class="toggle_btn">
+        <div></div>
+    </div>
+</nav>
+
+<div class="nav_mobile">
+    <!-- <span class="toggle">&times;</span> -->
+    <!--close-->
+
+    <div class="nav_links">
+        <?php wp_nav_menu(); ?>
+
+        <div class="booking_btn">
+            <a href="<?php echo get_theme_mod('custom-link-booking-url'); ?>" title="bokadirekt" target="_blank">
+
+                <img class="bokadirekt" src="https://foretag.bokadirekt.se/bokatid/BokaTid_Gron_MorkBakgrund_120px.png"
+                    alt="Boka tid" border="0" />
+            </a>
+        </div>
+    </div>
+
+</div>
