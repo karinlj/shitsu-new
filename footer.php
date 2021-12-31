79580af840c937a20c7c93ec9affb8ed5f66c0fc
Karin Ljunggren <kaljunggren@gmail.com>
2021-12-31 10:54:04 +0100

    footer responsive

--- footer.php
+++ footer.php
@@ -1,13 +1,14 @@
 <footer class="footer_container">
     <div class="footer_parent section_spacing_top_medium">
+        <?php
+        if (function_exists('acf_add_options_page')) { ?>
         <div class="container">
             <div class="row">
                 <?php
-                if (function_exists('acf_add_options_page')) {
                     $logo_id = get_field('logo', 'option');
                     $logo = wp_get_attachment_image_src($logo_id, 'full');
                     $alt_text = get_post_meta($logo_id, '_wp_attachment_image_alt', true); ?>
-                <div class="col-2">
+                <div class="col-lg-1 col-xl-2">
                     <?php if ($logo) {
                         ?>
                     <div class="logo">
@@ -23,7 +24,7 @@
                     if (have_rows('footer_fields', 'option')) {
                         while (have_rows('footer_fields', 'option')) {
                             the_row(); ?>
-                <div class="col">
+                <div class="col-lg-3 col-xl-2">
                     <h5 class="footer-fields-heading"><?php the_sub_field('heading'); ?></h5>
                     <p class="footer-fields-text"><?php the_sub_field('text'); ?></p>
                 </div>
@@ -31,7 +32,7 @@
                         }
                     } ?>
 
-                <div class="col">
+                <div class="col-lg-2 col-xl-2">
                     <div class="footer-list">
                         <?php
                             $book_button_url = get_field('footer_booking_button_url', 'option');
@@ -48,9 +49,7 @@
                     </div>
 
                 </div>
-                <?php
-                }
-                ?>
+
             </div>
         </div>
 
@@ -60,48 +59,30 @@
                     <div class="social_icons">
                         <ul class="social">
                             <?php
-                            //repeater
-                            if (have_rows('social_icons', 'option')) {
-                                while (have_rows('social_icons', 'option')) {
-                                    the_row();
+                                //repeater
+                                if (have_rows('social_icons', 'option')) {
+                                    while (have_rows('social_icons', 'option')) {
+                                        the_row();
 
-                                    $social_url = get_sub_field('social_url');
-                                    $social_site = get_sub_field('social_site'); ?>
+                                        $social_url = get_sub_field('social_url');
+                                        $social_site = get_sub_field('social_site'); ?>
                             <li class="social-item">
                                 <a href="<?php echo $social_url; ?>"><i
                                         class="fab fa-<?php echo $social_site; ?>"></i></a>
                             </li>
 
                             <?php
-                                }
-                            } ?>
+                                    }
+                                } ?>
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
+        <?php
+        }
+        ?>
     </div>
-
-    <?php
-    if (function_exists('acf_add_options_page')) { ?>
-
-    <!-- <div class="footer_parent section_spacing_top_small">
-                <?php
-                //repeater
-                if (have_rows('footer_fields', 'option')) {
-                    while (have_rows('footer_fields', 'option')) {
-                        the_row(); ?>
-                <div class="footer_child">
-                    <h5 class="footer-fields-heading"><?php the_sub_field('heading'); ?></h5>
-                    <p class="footer-fields-text"><?php the_sub_field('text'); ?></p>
-                </div>
-                <?php
-                    }
-                } ?>
-            </div> -->
-    <?php
-    }
-    ?>
 
     <div class="footer-bottom">
         <p class="footer-copy">&copy; <?php echo Date('Y'); ?> Shiatsurummet - All rights reserved.</p>
